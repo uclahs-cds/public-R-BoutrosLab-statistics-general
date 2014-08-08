926432ce54c680e7eb1d50c550a461ef01cb7096
ccooper <ccooper@noemail.ucla.edu>
2014-08-08 20:49:42 +0000

    moved BoutrosLab.package.utilities, BoutrosLab.pipeline.beadarray, BoutrosLab.pipeline.LTR BoutrosLab.statistics.general
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@33055 aa8168d8-c5e5-4185-88a0-f3e152ed764a

---
+++ R/get.foldchange.R
@@ -0,0 +1,23 @@
+# The BoutrosLab.statistics.general package is copyright (c) 2012 Ontario Institute for Cancer Research (OICR)
+# This package and its accompanying libraries is free software; you can redistribute it and/or modify it under the terms of the GPL
+# (either version 1, or at your option, any later version) or the Artistic License 2.0.  Refer to LICENSE for the full license text.
+# OICR makes no representations whatsoever as to the SOFTWARE contained herein.  It is experimental in nature and is provided WITHOUT
+# WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE OR ANY OTHER WARRANTY, EXPRESS OR IMPLIED. OICR MAKES NO REPRESENTATION
+# OR WARRANTY THAT THE USE OF THIS SOFTWARE WILL NOT INFRINGE ANY PATENT OR OTHER PROPRIETARY RIGHT.
+# By downloading this SOFTWARE, your Institution hereby indemnifies OICR against any loss, claim, damage or liability, of whatsoever kind or
+# nature, which may arise from your Institution's respective use, handling or storage of the SOFTWARE.
+# If publications result from research using this SOFTWARE, we ask that the Ontario Institute for Cancer Research be acknowledged and/or
+# credit be given to OICR scientists, as scientifically appropriate.
+
+get.foldchange <- function(x, group1, group2, logged = TRUE) {
+
+	group1 <- as.numeric(x[group1]);
+	group2 <- as.numeric(x[group2]);
+
+	if (TRUE == logged) {
+		return( mean(group2, na.rm = TRUE) - mean(group1, na.rm = TRUE) );
+		}
+	else if (FALSE == logged) {
+		return( mean(group2, na.rm = TRUE) / mean(group1, na.rm = TRUE) );
+		}
+	}
